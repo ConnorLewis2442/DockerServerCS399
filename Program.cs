be46e2f8aaf5abd6644dfd87790af133827bd4cb
Connor Lewis <81784772+ConnorLewis2442@users.noreply.github.com>
2025-10-02 13:50:16 -0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 using CS397.Trace;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
+
+
 class Program
 {
     const string serviceName = "monitored-docker-web-service";
@@ -13,6 +15,41 @@ class Program
         _tracer = TracerProvider.Default.GetTracer(serviceName);
     }
 
+    
+    private async Task UploadFile(HttpContext context)
+{
+    var span = _tracer.StartSpan("UploadFile");
+    try
+    {
+        var userId = context.Request.Query["userId"].ToString();
+        span.SetAttribute("userId", userId);
+
+        var file = context.Request.Form.Files.FirstOrDefault();
+        if (file == null)
+        {
+            context.Response.StatusCode = 400;
+            await context.Response.WriteAsync("No file uploaded.");
+            return;
+        }
+
+        span.SetAttribute("fileName", file.FileName);
+
+        // Your upload logic here: save file to Blob, metadata to Cosmos DB, etc.
+
+        await context.Response.WriteAsync("Upload successful");
+    }
+    catch (Exception e)
+    {
+        span.SetAttribute("error", true);
+        span.SetAttribute("error.message", e.Message);
+        context.Response.StatusCode = 500;
+        await context.Response.WriteAsync("Internal server error");
+    }
+    finally
+    {
+        span.End();
+    }
+}
     private async Task HelloWorldDelegate(HttpContext context)
     {
         // OpenTelemetry uses concepts of "span" to correlate all messages together
@@ -32,7 +69,7 @@ class Program
         {
             await context.Response.WriteAsync("Hello, World!");
         }
-        catch(Exception e)
+        catch (Exception e)
         {
             // If any error happend via an exception, we can include that information
             // in the log. Including the stack trace can be either useful or very
@@ -40,7 +77,7 @@ class Program
             currentSpan.SetAttribute("error", true);
             currentSpan.SetAttribute("error.message", e.Message);
             currentSpan.SetAttribute("error.stacktrace", e.StackTrace);
-            
+
             // 500 is the typical status code for an internal server error
             // We got an unhandled exception, so we don't know what went wrong
             // Hence we log the information and return a 500 status code
@@ -84,6 +121,7 @@ class Program
         app.MapGet("/", instance.HelloWorldDelegate);
         app.MapGet("/hello", instance.HelloWorldDelegate);
         app.MapGet("/goodbye", instance.GoodbyeWorldDelegate);
+        app.MapGet("/UploadFile", instance.GoodbyeWorldDelegate);
 
         app.Run();
 
