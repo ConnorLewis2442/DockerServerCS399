87a2de462f5bfc722721ca3b43b671d360336561
Connor Lewis <81784772+ConnorLewis2442@users.noreply.github.com>
2025-10-02 14:34:38 -0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -18,8 +18,9 @@ class Program
         _tracer = TracerProvider.Default.GetTracer(serviceName);
 
         // Connect to Azure Blob Storage container (set connection string & container name accordingly)
-        string connectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
-        string containerName = "uploads"; // your container name
+        string connectionString = Environment.GetEnvironmentVariable("DefaultEndpointsProtocol=https;AccountName=blobstorage399;AccountKey=MEhoHND0Tn7HB6bNG8u7JzT65ihAhT3pv+Tp2dNrtWgv1uHeyY3x4cx20obZKBdsDA8peQKh+Xlt+AStLyUk9g==;EndpointSuffix=core.windows.net");
+        string containerName = "blobstorage399"; // your container name
+
         _blobContainerClient = new BlobContainerClient(connectionString, containerName);
         _blobContainerClient.CreateIfNotExists();  // Ensure container exists
     }
@@ -29,7 +30,7 @@ class Program
 {
     var span = _tracer.StartSpan("UploadFile");
     try
-    {
+    { 
         var userId = context.Request.Query["userId"].ToString();
         span.SetAttribute("userId", userId);
 
