5a82c900231d7cf32ea8663f14ba635e1772f444
Connor Lewis <81784772+ConnorLewis2442@users.noreply.github.com>
2025-12-04 17:04:45 -0800

    Update FileServerHandlers.cs

--- FileServerHandlers.cs
+++ FileServerHandlers.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AzureFileServer.Auth; // make User.Username visible
 
 public class FileServerHandlers
 {
